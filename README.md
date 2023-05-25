60a8546152b5d8901a4e1558322aca029eba2f9e
ViliFidosheva <123537059+ViliFidosheva@users.noreply.github.com>
2023-05-26 00:17:44 +0300

    new changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # First-Steps
 These are my first steps in GitHub
+
+# (This is a demo created by Vili Fidosheva)
--- solution.py
+++ solution.py
@@ -1,6 +1,9 @@
 first_number = int(input())
-second_nuber = int(input())
+second_number = int(input())
 
 result = (first_number + second_number) * first_number
 
 print(result)
+print(result ** 2)
+print(result % 2)
+
